e9f7a77bde98a4cdb305db280fe88e5e7b23c394
Anne-Sophie Mertgen <anne-sophie.mertgen@anne-sophies-macbook-pro.local>
2018-04-13 15:34:33 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Anne-Sophie Mertgen on 13.04.18.
+//  Copyright © 2018 Anne-Sophie Mertgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
